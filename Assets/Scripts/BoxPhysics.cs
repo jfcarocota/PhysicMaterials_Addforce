f3ecf3fa4ac33ca035fe8e1ecd3cf3e4536f1095
jfcarocota <jfcarocota@gmail.com>
2019-02-20 08:38:07 -0700

    addforce added

---
+++ Assets/Scripts/BoxPhysics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPhysics : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.up * 500, ForceMode2D.Force);
+    }
+}
